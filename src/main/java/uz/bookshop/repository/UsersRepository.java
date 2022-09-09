e9beecc16b671e9a8f28727dc8b042ff8c116b63
Jakhongir <jakhongirsherjonov@gmail.com>
2022-09-09 16:48:15 +0500

    spring-security

---
+++ src/main/java/uz/bookshop/repository/UsersRepository.java
@@ -0,0 +1,13 @@
+package uz.bookshop.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import uz.bookshop.entity.model.Users;
+
+@Repository
+public interface UsersRepository extends CrudRepository<Users, Long> {
+    //
+    Users findByName(String user);
+    Users findByLogin(String login);
+    boolean existsByLogin(String login);
+}
